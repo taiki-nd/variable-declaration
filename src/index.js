3c60e9d7308d753b940049d77b6a6473ff91b330
Taiki Noda <84260062+taiki-nd@users.noreply.github.com>
2021-10-13 18:09:16 +0900

    letについて

--- src/index.js
+++ src/index.js
@@ -13,3 +13,16 @@ console.log(val1);
 // var変数の再宣言
 var val1 = "var変数の再宣言";
 console.log(val1);
+
+//-----------------------------
+
+let val2 = "let変数";
+console.log(val2);
+
+// letは上書き可能
+
+val2 = "let変数を上書き";
+console.log(val2);
+
+// letは再宣言不可
+//let val2 = "let変数の再宣言";
